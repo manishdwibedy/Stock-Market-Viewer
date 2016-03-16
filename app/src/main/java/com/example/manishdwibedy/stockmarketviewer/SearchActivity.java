5c3d79a08f619f0d774964a8310b042d95230e30
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-15 17:30:38 -0700

    Removed commented code.

--- app/src/main/java/com/example/manishdwibedy/stockmarketviewer/SearchActivity.java
+++ app/src/main/java/com/example/manishdwibedy/stockmarketviewer/SearchActivity.java
@@ -13,23 +13,6 @@ import android.view.MenuInflater;
 public class SearchActivity extends AppCompatActivity {
     private final String TAG = "SearchActivity";
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_search);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
