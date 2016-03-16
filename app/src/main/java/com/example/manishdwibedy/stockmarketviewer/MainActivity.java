a916fac993af09cd430dd582155b49fa1f9ef0b2
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-15 22:23:23 -0700

    Removing unused code.

--- app/src/main/java/com/example/manishdwibedy/stockmarketviewer/MainActivity.java
+++ app/src/main/java/com/example/manishdwibedy/stockmarketviewer/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.manishdwibedy.stockmarketviewer;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -13,12 +12,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity{
 
-    private static final String[] SUGGESTIONS = {
-            "Bauru", "Sao Paulo", "Rio de Janeiro",
-            "Bahia", "Mato Grosso", "Minas Gerais",
-            "Tocantins", "Rio Grande do Sul"
-    };
-    private SimpleCursorAdapter mAdapter;
     private final String TAG = "MainActivity";
     SearchView searchView;
 
