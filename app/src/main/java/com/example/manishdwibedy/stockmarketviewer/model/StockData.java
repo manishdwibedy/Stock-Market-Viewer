8d1598b9539395e3d1b011a3c71c8cff8e472d09
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-20 13:49:16 -0700

    Getting the stock data by hitting the API. Also tested the working.

---
+++ app/src/main/java/com/example/manishdwibedy/stockmarketviewer/model/StockData.java
@@ -0,0 +1,187 @@
+package com.example.manishdwibedy.stockmarketviewer.model;
+
+/**
+ * Created by manishdwibedy on 3/20/16.
+ */
+public class StockData
+{
+    private String Open;
+
+    private String Low;
+
+    private String Change;
+
+    private String Volume;
+
+    private String Timestamp;
+
+    private String Name;
+
+    private String Status;
+
+    private String ChangePercent;
+
+    private String Symbol;
+
+    private String LastPrice;
+
+    private String MSDate;
+
+    private String ChangeYTD;
+
+    private String MarketCap;
+
+    private String ChangePercentYTD;
+
+    private String High;
+
+    public String getOpen ()
+    {
+        return Open;
+    }
+
+    public void setOpen (String Open)
+    {
+        this.Open = Open;
+    }
+
+    public String getLow ()
+    {
+        return Low;
+    }
+
+    public void setLow (String Low)
+    {
+        this.Low = Low;
+    }
+
+    public String getChange ()
+    {
+        return Change;
+    }
+
+    public void setChange (String Change)
+    {
+        this.Change = Change;
+    }
+
+    public String getVolume ()
+    {
+        return Volume;
+    }
+
+    public void setVolume (String Volume)
+    {
+        this.Volume = Volume;
+    }
+
+    public String getTimestamp ()
+    {
+        return Timestamp;
+    }
+
+    public void setTimestamp (String Timestamp)
+    {
+        this.Timestamp = Timestamp;
+    }
+
+    public String getName ()
+    {
+        return Name;
+    }
+
+    public void setName (String Name)
+    {
+        this.Name = Name;
+    }
+
+    public String getStatus ()
+    {
+        return Status;
+    }
+
+    public void setStatus (String Status)
+    {
+        this.Status = Status;
+    }
+
+    public String getChangePercent ()
+    {
+        return ChangePercent;
+    }
+
+    public void setChangePercent (String ChangePercent)
+    {
+        this.ChangePercent = ChangePercent;
+    }
+
+    public String getSymbol ()
+    {
+        return Symbol;
+    }
+
+    public void setSymbol (String Symbol)
+    {
+        this.Symbol = Symbol;
+    }
+
+    public String getLastPrice ()
+    {
+        return LastPrice;
+    }
+
+    public void setLastPrice (String LastPrice)
+    {
+        this.LastPrice = LastPrice;
+    }
+
+    public String getMSDate ()
+    {
+        return MSDate;
+    }
+
+    public void setMSDate (String MSDate)
+    {
+        this.MSDate = MSDate;
+    }
+
+    public String getChangeYTD ()
+    {
+        return ChangeYTD;
+    }
+
+    public void setChangeYTD (String ChangeYTD)
+    {
+        this.ChangeYTD = ChangeYTD;
+    }
+
+    public String getMarketCap ()
+    {
+        return MarketCap;
+    }
+
+    public void setMarketCap (String MarketCap)
+    {
+        this.MarketCap = MarketCap;
+    }
+
+    public String getChangePercentYTD ()
+    {
+        return ChangePercentYTD;
+    }
+
+    public void setChangePercentYTD (String ChangePercentYTD)
+    {
+        this.ChangePercentYTD = ChangePercentYTD;
+    }
+
+    public String getHigh ()
+    {
+        return High;
+    }
+
+    public void setHigh (String High)
+    {
+        this.High = High;
+    }
+}
---
+++ app/src/main/java/com/example/manishdwibedy/stockmarketviewer/util/GetStockData.java
@@ -0,0 +1,29 @@
+package com.example.manishdwibedy.stockmarketviewer.util;
+
+import com.example.manishdwibedy.stockmarketviewer.model.StockData;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Created by manishdwibedy on 3/20/16.
+ */
+public class GetStockData {
+    public static StockData getStockData(String symbol){
+        //List<Stock> stocks = new ArrayList<Stock>();
+        StockData stockData = null;
+        String baseURL = "https://stock-app-csci.appspot.com/api.php?operation=getstock&symbol=";
+        try {
+            String response = HandleGETRequests.sendGet(baseURL + symbol);
+            Gson gson = new GsonBuilder().create();
+            stockData = gson.fromJson(response, StockData.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stockData;
+    }
+
+    public static void main(String[] args){
+        StockData data = getStockData("AAPL");
+        System.out.print(data.getChange());
+    }
+}
