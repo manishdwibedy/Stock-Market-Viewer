421cd913f3b8b397fdfd22b647a131408497e922
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-24 15:58:49 -0700

    Converted the news title to the link.

--- app/src/main/java/com/example/manishdwibedy/stockmarketviewer/adapter/StockNewsFeedAdapter.java
+++ app/src/main/java/com/example/manishdwibedy/stockmarketviewer/adapter/StockNewsFeedAdapter.java
@@ -1,6 +1,7 @@
 package com.example.manishdwibedy.stockmarketviewer.adapter;
 
 import android.app.Activity;
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import com.example.manishdwibedy.stockmarketviewer.R;
 import com.example.manishdwibedy.stockmarketviewer.model.stock.feednews.StockNewsAdapterDetails;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4;
 
@@ -20,11 +22,23 @@ import static org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4;
 public class StockNewsFeedAdapter extends ArrayAdapter<StockNewsAdapterDetails> {
     private Activity context;
     private List<StockNewsAdapterDetails> data;
+    private Linkify.MatchFilter transformFilter;
+    private Pattern pattern = Pattern.compile("\\[a-zA-Z]+");
 
     public StockNewsFeedAdapter(Activity context, List<StockNewsAdapterDetails> data) {
         super(context, R.layout.current_stock_news_feed, data);
         this.context = context;
         this.data = data;
+
+        // Convert the text to link
+        // starting from index 0
+        this.transformFilter = new Linkify.MatchFilter() {
+            @Override
+            public boolean acceptMatch(CharSequence cs, int start, int end) {
+                return start >= 0;
+            }
+        };
+
 
     }
 
@@ -38,11 +52,24 @@ public class StockNewsFeedAdapter extends ArrayAdapter<StockNewsAdapterDetails> 
         TextView newsPublisher = (TextView) listViewItem.findViewById(R.id.stockNewsPublisher);
         TextView newsDate = (TextView) listViewItem.findViewById(R.id.stockNewsDate);
 
-
         StockNewsAdapterDetails detail = data.get(position);
+
+        // Set the news title
         newsTitle.setText(detail.getTitle());
+
+        // Set the news link
+        String newsLink = "";
+
+        // Adding the link to the news title
+        Linkify.addLinks(newsTitle, this.pattern, newsLink, this.transformFilter, null);
+
+        // Set the news content
         newsContent.setText(unescapeHtml4(detail.getContent()));
+
+        // Set the new's publisher
         newsPublisher.setText(detail.getPublisher());
+
+        // Set the new's date
         newsDate.setText(detail.getPublishedDate());
 
         return listViewItem;
