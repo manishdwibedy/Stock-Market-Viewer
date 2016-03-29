3b019bf58837b4cd3865eb5486bf96142bcd5a87
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-29 01:33:21 -0700

    Removing commented code.

--- app/src/main/java/com/example/manishdwibedy/stockmarketviewer/MainActivity.java
+++ app/src/main/java/com/example/manishdwibedy/stockmarketviewer/MainActivity.java
@@ -89,16 +89,6 @@ public class MainActivity extends AppCompatActivity{
 
         setupFavorites();
 
-//        //Create Array Adapter
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, languages);
-//        //Find TextView control
-//        AutoCompleteTextView acTextView = (AutoCompleteTextView) findViewById(R.id.languages);
-//        //Set the number of characters the user must type before the drop down list is shown
-//        acTextView.setThreshold(1);
-//        //Set the adapter
-//        acTextView.setAdapter(adapter);
-
-
         final DelayAutoCompleteTextView bookTitle = (DelayAutoCompleteTextView) findViewById(R.id.et_book_title);
         bookTitle.setThreshold(3);
 
